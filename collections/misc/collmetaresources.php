b31fd2787d1ba7b8fbd3478344404eb9a14ed44c
Katie Pearson <katelin.d.pearson24@gmail.com>
2022-05-17 13:59:34 -0700

    Revert "Attempt to change language in parentheses"
    This reverts commit 3135bafc2cfb1537dc94c311fadceaafb1597c57.

--- collections/misc/collmetaresources.php
+++ collections/misc/collmetaresources.php
@@ -89,7 +89,7 @@ if($collid && $isEditor){
 						elseif($langCode == 'pr') $langStr = (isset($LANG['PORTUGUESE'])?$LANG['PORTUGUESE']:'Portuguese');
 						?>
 						<div class="field-block" style="">
-							<span class="field-label"><?php echo (isset($LANG['CAPTION_OVERRIDE'])?$LANG['CAPTION_OVERRIDE']:'Caption override').' ('.$langCode.'):'; ?></span>
+							<span class="field-label"><?php echo (isset($LANG['CAPTION_OVERRIDE'])?$LANG['CAPTION_OVERRIDE']:'Caption override').' ('.$langStr.'):'; ?></span>
 							<span class="field-elem">
 								<input name="title-<?php echo $langCode; ?>" type="text" value="Homepage" />
 							</span>
